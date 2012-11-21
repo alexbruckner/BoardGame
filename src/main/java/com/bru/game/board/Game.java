a1c7c21c466db349159c4025bc712240edcea6c1
alexbruckner <bruckner.alex@gmail.com>
2012-11-21 21:16:36 +0000

    Test cases

---
+++ src/main/java/com/bru/game/board/Game.java
@@ -0,0 +1,27 @@
+package com.bru.game.board;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alex
+ * Date: 21/11/12
+ * Time: 21:13
+ */
+public class Game {
+
+	//select game type
+
+	//select initial setup
+
+	//attach rules per move
+
+	//build board
+
+	//setup initial pieces if any
+
+	//create interface for placing/moving pieces depending on rules
+
+	//present per move
+
+	//TODO
+
+}
